826e599282f313488ef0cb2672294ff94e5022c6
Akash-Maji <akashmaji556@gmail.com>
2024-02-02 18:58:33 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import profile from '/profile.png'
+import { Link } from 'react-router-dom';
+// import './home.css'
+
+export default function Home() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const handleData = async () => {
+            try {
+                const res = await axios.get('https://602e7c2c4410730017c50b9d.mockapi.io/users');
+                // console.log(res.data);
+                setUsers(res.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        handleData();
+    }, [])
+
+    const addDefaultSrc = (e) => {
+        e.target.src = profile;
+    }
+
+    return (
+        <section className='bg-dark'>
+            <div className="container py-5  justify-content-center " style={{backgroundImage: "url('/user_bg.jpg')",backgroundSize: "auto"}}>
+                <div className="d-flex justify-content-center ">
+                    <div className="col-md-8 people-nearby">
+                        <h1 className='text-white text-center'>Users</h1>
+                        {users.map((user) => (
+                            <Link to={`/profile/${user.id}`} className='text-decoration-none text-white'>
+                                <div key={user.profile.username} className="nearby-user border-top border-bottom align-items-center p-3 ">
+                                    <div className="row">
+                                        <div className="col-2 col-md-2 p-1">
+                                            <img src={user.avatar} onError={addDefaultSrc} className='rounded-circle' height={"100%"} width={"100%"} alt={user.profile.firstName} />
+                                        </div>
+                                        <div className="col-7 col-md-7 d-flex align-items-center">
+                                            <div className=''>
+                                                <h5>{user.profile.firstName} {user.profile.lastName}</h5>
+                                                <p className='mb-0'>{user.jobTitle}</p>
+                                                <p className='mb-0'>{user.profile.email}</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-3 col-md-3 d-flex align-items-center">
+                                            <button className="btn btn-primary pull-right">Details</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import profile from '/profile.png';
+import axios from 'axios';
+
+export default function Profile() {
+    const [users, setUsers] = useState([]);
+    const params = useParams();
+
+    useEffect(() => {
+        const handleData = async () => {
+            try {
+                const res = await axios.get(`https://602e7c2c4410730017c50b9d.mockapi.io/users/${params.id}`);
+                console.log(res.data);
+                setUsers(res.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        handleData();
+    }, [])
+
+    const addDefaultSrc = (e) => {
+        e.target.src = `${profile}`;
+    }
+
+    return (
+        <div className='bg-dark vh-100 d-flex' style={{backgroundImage: "url('/bg.png')", backgroundSize: "cover"}}>
+            <div className="container d-flex justify-content-center align-items-center">
+                <div className="card p-1 my-5">
+                    <div className="upper w-100">
+                        <img src="/cover_img.jpg" className="img-fluid w-100" />
+                    </div>
+                    <div className="user text-center position-relative">
+                        <div className="profile position-absolute rounded-circle" style={{ top: "-50px", left: "40%", border: "3px solid #fff" }}>
+                            <img src={users.avatar} onError={addDefaultSrc} className="rounded-circle" width="80" />
+                        </div>
+                    </div>
+                    <div className="mt-5 text-center">
+                        {users.profile ?
+                            <h4 className="mb-0">{users.profile.firstName} {users.profile.lastName}</h4> :
+                            <h4 className="mb-0"></h4>
+                        }
+                        {users.profile ?
+                            <span className="mb-0">@{users.profile.username}</span> :
+                            <h4 className="mb-0"></h4>
+                        }
+                        <span className="text-muted d-block mb-2">{users.jobTitle}</span>
+                        <div className="d-flex justify-content-center align-items-center my-4 px-4">
+                            <div>
+                                <p>{users.Bio}</p>
+                                {users.profile ?
+                                    <p>{users.profile.email}</p> :
+                                    <p>Akash@gmail.com</p>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
